ce6880fa9165833bcdb0ecad6bb612c42f506990
synesdeija <thedeija@gmail.com>
2023-04-05 14:16:55 -0500

    components added for About, AddTask, Button, Footer, Header, Task and Tasks

---
+++ src/components/Header.js
@@ -0,0 +1,35 @@
+import PropTypes from 'prop-types'
+import { useLocation } from 'react-router-dom'
+import Button from './Button'
+
+const Header = ({ title, onAdd, showAdd }) => {
+  const location = useLocation()
+
+  return (
+    <header className="header">
+      <h1>{title}</h1>
+      {location.pathname === '/' && (
+        <Button
+          color={showAdd ? 'red' : 'green'}
+          text={showAdd ? 'close' : 'add'}
+          onClick={onAdd}
+        />
+      )}
+    </header>
+  )
+}
+
+Header.defaultProps = {
+  title: 'Task Tracker',
+}
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+}
+
+//CSS in JS
+// const headingStyle = {
+//   color:'purple', backgroundColor: 'gray'
+// }
+
+export default Header
